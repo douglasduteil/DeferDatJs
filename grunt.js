8f5740f78a2ed0a4184d8a29acc9ef05d28c47ab
douglasduteil <douglasduteil@gmail.com>
2013-01-04 21:52:27 +0100

    Config for travis

--- grunt.js
+++ grunt.js
@@ -27,25 +27,4 @@ module.exports = function(grunt) {
     // Travis CI task.
     grunt.registerTask('travis', 'coffee lint');
 
-
-    grunt.registerTask('test', 'run tests (make sure server task is run first)', function () {
-        var done = this.async();
-        grunt.utils.spawn({
-            cmd: process.platform === 'win32' ? 'testacular.cmd' : 'testacular',
-            args: process.env.TRAVIS ? ['start', 'test/test-config.js', '--single-run', '--no-auto-watch', '--reporter=dots', '--browsers=Firefox'] : ['run']
-        }, function (error, result, code) {
-            if (error) {
-                grunt.warn("Make sure the testacular server is online: run `grunt server`.\n" +
-                    "Also make sure you have a browser open to http://localhost:8080/.\n" +
-                    error.stdout + error.stderr);
-                //the testacular runner somehow modifies the files if it errors(??).
-                //this causes grunt's watch task to re-fire itself constantly,
-                //unless we wait for a sec
-                setTimeout(done, 1000);
-            } else {
-                grunt.log.write(result.stdout);
-                done();
-            }
-        });
-    });
 };
